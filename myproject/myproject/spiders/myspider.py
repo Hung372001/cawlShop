fdb342c2ed5c70769b34932596cc8649f382e118
VietHung1 <hunghv@dolphin-technologies.net>
2024-02-24 20:10:58 +0700

    update list category

--- myproject/myproject/spiders/myspider.py
+++ myproject/myproject/spiders/myspider.py
@@ -1,22 +1,37 @@
 import scrapy
 from scrapy.selector import Selector
+import re
 
 
 class QuotesSpider(scrapy.Spider):
     name = "myspider"
     start_urls = ["https://dirtycoins.vn/shop"]
 
+
+
+
+
     def parse(self, response):
+        list_category = ['T-shirt','Pol-shirt','Shirt','SWEATERS','CARDIGANS','Bag','Jacket','Pack','Hoodie','Beanie','Combo','Jersey','Patch '
+            ,'PANTS','Shorts','DRESSES','SKIRTS','Backpack','CROSSBODY BAGS','BOWLER BAGS','Cap','HATS','SLIDES','WALLETS','UNDERWEAR','MASKS','Jeans' ]
+
         for res in response.css("div.single-product"):
             img = res.css("img.lazyload::attr(src)").get()
             title = res.css("div.product-content>h3>a::text").get()
             price = res.css("span.regular-price::text").get()
+            category = None
+            for word in list_category:
+                if word in title:
+                    category = word
+
+
+
             item = {}
             item['img'] = img
             item['title'] = title
             item['price'] = price
+            item['category'] = category
             yield item
 
-# next_page = response.css("li.next a::attr(href)").get()
-# if next_page is not None:
-#     yield response.follow(next_page, self.parse)
+
+
